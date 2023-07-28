5698dd219539bc90a5101c8e7a9c104c4fffa199
matia6170 <matia6170@gmail.com>
2023-07-28 01:40:38 -0700

    Fixed a bug where a change in a motor would affect another motor. This was caused by the fact that there was a single PID fucntion responsible for both motors. The way I implemented it used global variables to store the error sum(integral) and previous error for the derivative term. This caused change in one motor, for example, holding down a motor or changing the PID constants in one of the PID functions, to affect the output of the other motor. This issue was fixed by creating dedicated PID functions for each of the left and right motors.

--- PIDBot.ino
+++ PIDBot.ino
@@ -1,3 +1,14 @@
+/*
+MIT License
+
+Copyright July 2023 Hyunwoo Choi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 //MAX RPM 250
 
 const int ENCR_CLK = 2;
@@ -18,20 +29,24 @@ float RPM_R = 0;
 float RPM_L = 0;
 
 //Motor RPM calculation related
-long currT=millis();
+long currT=micros();
 long prevT=0; 
 long MOTL_ENC_CNT_PREV = 0;
 long MOTR_ENC_CNT_PREV = 0;
 float deltaT = 0;
 
 //PID
-float eintegral = 0;
-float prevE = 0;
+float eintegral_L = 0;
+float prevE_L = 0;
+float eintegral_R = 0;
+float prevE_R = 0;
 
 /* Physical Properties of the Robot */
 const int circumference = 70;
 const int ticksPerRev = 400;
 const float ticksPerCm = ticksPerRev/circumference;
+
+long startTimer = micros();
 
 void ISR_R() {
 
@@ -108,6 +123,12 @@ void setMotorL(int dir, int speed){
       break;
   }
 }
+void stop(){
+    setMotorR(0,0);
+    setMotorL(0,0);
+
+
+}
 
 int CMtoTicks(int cm) {
   return ticksPerCm * cm;
@@ -132,28 +153,47 @@ void calcRPM(){
   prevT = currT;
 }
 
-void PID(float vt, float rpm, int *pwr, int *dir){
+void PID_L(float vt, float rpm, int *pwr, int *dir){
   float kp = 1;
   float ki = 10;
   float kd = 0.05;
 
   float e = vt-rpm;
 
-  eintegral = eintegral + e * deltaT;
+  eintegral_L = eintegral_L + e * deltaT;
 
-  float u = kp*e + kd*prevE + ki*eintegral;
+  float u = kp*e + kd*prevE_L + ki*eintegral_L;
 
-  prevE = e;
+  prevE_L = e;
 
   if (u<0)
     *dir=-1;
 
   *pwr = (int) fabs(u);
-  if(*pwr>255){
+  if(*pwr>255)
     *pwr = 255;
-  }
   
+}
+void PID_R(float vt, float rpm, int *pwr, int *dir){
+  float kp = 1;
+  float ki = 10;
+  float kd = 0.05;
 
+  float e = vt-rpm;
+
+  eintegral_R = eintegral_R + e * deltaT;
+
+  float u = kp*e + kd*prevE_R + ki*eintegral_R;
+
+  prevE_R = e;
+
+  if (u<0)
+    *dir=-1;
+
+  *pwr = (int) fabs(u);
+  if(*pwr>255)
+    *pwr = 255;
+  
 }
 
 void setup() {
@@ -173,38 +213,49 @@ void setup() {
 
 
 void loop() {
+  float vt = 100;
+  if ((currT - startTimer) < 40*1000000){
+    
+    calcRPM();
 
-  calcRPM();
+    // //set a target
+     //vt = 100 *(sin(currT/1e6)>0)+100;
+    
+    int pwr_l = 0;
+    int dir_l = 1;
+    PID_L(vt, RPM_L, &pwr_l, &dir_l);
+    setMotorL(dir_l, pwr_l);
 
-  // //set a target
-  float vt = 100 *(sin(currT/1e6)>0)+250;
-  
+    int pwr_r = 0;
+    int dir_r = 1;
+    PID_R(vt, RPM_R, &pwr_r, &dir_r);
+    setMotorR(dir_r, pwr_r);
+  }else{
+    stop();
+  }
 
 
-  int pwr_l = 0;
-  int dir_l = 1;
-  PID(vt, RPM_L, &pwr_l, &dir_l);
-  setMotorL(dir_l, pwr_l);
 
-  // int pwr_r = 0;
-  // int dir_r = 1;
-  // PID(vt, RPM_R, &pwr_r, &dir_r);
-  // setMotorR(dir_r, pwr_r);
 
   Serial.print("0 ");
-  Serial.print("500 ");
+  Serial.print("300 ");
   Serial.print(vt);
   Serial.print(" ");
   Serial.print(RPM_L);
   Serial.print(" ");
-  Serial.print(pwr_l);
+  Serial.print(RPM_R);
+  Serial.println("");
 
 
-  Serial.println();
+
+
   // currT = millis();
   // if(currT - start > 2000){
   //   analogWrite(MOTR_FW, 0);
   //   analogWrite(MOTL_FW, 0);
   // }
+
+  
+    
 
 }
